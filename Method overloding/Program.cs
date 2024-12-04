9c2a2b475d6fcfe442b76ca617c13463d766325d
kirtigalani <kittugalani@gmail.com>
2024-12-04 19:14:17 +0530

    Add project files.

---
+++ Method overloding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Method_overloding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void add(int FN, int SN , params int[] TN)
+        {
+            Console.WriteLine("sum: {0}", FN + SN);
+        }
+        public static void add(int FN, int SN, int[] TN)
+        {
+            Console.WriteLine("sum: {0}", FN + SN);
+        }
+    }
+}
